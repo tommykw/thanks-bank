c3ff236d32f1ce8888eec7bcd1e128792c8072db
kenji tomita <destinys.lotus.1108mn@gmail.com>
2019-06-29 12:55:14 +0900

    Add repository

---
+++ src/model/Emoji.kt
@@ -0,0 +1,5 @@
+package com.tommykw.model
+
+data class Emoji(val name: String) {
+    var id: Int? = null
+}
---
+++ src/repository/Repository.kt
@@ -0,0 +1,12 @@
+package com.tommykw.repository
+
+import com.tommykw.model.Emoji
+
+interface Repository {
+    suspend fun add(emoji: Emoji): Emoji
+    suspend fun emoji(id: Int): Emoji?
+    suspend fun emojis(): List<Emoji>
+    suspend fun remove(id: Int): Boolean
+    suspend fun remove(emoji: Emoji): Boolean
+    suspend fun clear()
+}
