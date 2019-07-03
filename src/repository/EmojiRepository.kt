92005164d51f665e8fe1dcca9baf8f31f9b856b6
kenji tomita <destinys.lotus.1108mn@gmail.com>
2019-07-03 19:39:24 +0900

    Fix no transation in context

--- src/repository/EmojiRepository.kt
+++ src/repository/EmojiRepository.kt
@@ -2,10 +2,7 @@ package com.tommykw.repository
 
 import com.tommykw.model.Emoji
 import com.tommykw.model.EmojiData
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.deleteAll
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class EmojiRepository : Repository {
@@ -18,19 +15,27 @@ class EmojiRepository : Repository {
     }
 
     override suspend fun emoji(id: Int): Emoji? {
-        DatabaseFactory.dbQuery {
-            
+        return DatabaseFactory.dbQuery {
+            EmojiData.select {
+                (EmojiData.id eq id)
+            }.mapNotNull { toEmoji(it) }
+                .singleOrNull()
         }
-        return emoji(id)
     }
 
     override suspend fun emojis(): List<Emoji> {
-        return EmojiData.selectAll().map { toEmoji(it) }
+        return transaction {
+            EmojiData.selectAll().map { toEmoji(it) }
+        }
     }
 
     override suspend fun remove(id: Int): Boolean {
         if (emoji(id) == null) {
             throw IllegalArgumentException("No phrase found for id $id")
+        }
+
+        return DatabaseFactory.dbQuery {
+            EmojiData.deleteWhere { EmojiData.id eq id } > 0
         }
     }
 
