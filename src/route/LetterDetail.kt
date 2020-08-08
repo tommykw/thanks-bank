dfcf55a966a48304a08b5ddf1e7f0f7fc56b673c
tommykw <destinys.lotus.1108mn@gmail.com>
2020-08-08 16:47:51 +0900

    Assign user data by user api

--- src/route/LetterDetail.kt
+++ src/route/LetterDetail.kt
@@ -19,8 +19,19 @@ fun Route.letterDetail() {
     get<LetterDetail> { listing ->
         val repository by kodein().instance<ThankRepository>()
         val thank = repository.getThank(listing.thankId)
+        val members = repository.getSlackMembers()
         var reactions: List<ThankReaction>? = null
         var threads: List<Thank>? = null
+
+        fun idToRealName(slackId: String): String {
+            val res = members.members.find { it.id == slackId }
+            return res?.real_name ?: ""
+        }
+
+        fun idToProfileImage(slackId: String): String {
+            val res = members.members.find { it.id == slackId }
+            return res?.profile?.image_512 ?: ""
+        }
 
         thank.slackPostId?.let { slackPostId ->
             println("!!!!!!!! slackPostId " + slackPostId)
@@ -28,6 +39,26 @@ fun Route.letterDetail() {
             println("!!!!!!! reactions " + reactions)
             threads = repository.getThreads(slackPostId)
             println("!!!!!!! threads " + threads)
+
+            thank.realName = idToRealName(thank.slackUserId)
+            thank.targetSlackUserId?.let {
+                thank.targetRealName = idToRealName(it)
+            }
+            thank.userImage = idToProfileImage(thank.slackUserId)
+            thank.targetSlackUserId?.let {
+                thank.targetUserImage = idToProfileImage(it)
+            }
+
+            threads?.map { thread ->
+                thread.realName = idToRealName(thread.slackUserId)
+                thread.targetSlackUserId?.let {
+                    thread.targetRealName = idToRealName(it)
+                }
+                thread.userImage = idToProfileImage(thread.slackUserId)
+                thread.targetSlackUserId?.let {
+                    thread.targetUserImage = idToProfileImage(it)
+                }
+            }
         }
 
         call.respond(
