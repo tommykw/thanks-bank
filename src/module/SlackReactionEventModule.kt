9afa9da60fd9d59b481cb850081ea7bfe5eb9ba3
tommykw <destinys.lotus.1108mn@gmail.com>
2020-08-09 10:58:51 +0900

    Debug reaction remove event

--- src/module/SlackReactionEventModule.kt
+++ src/module/SlackReactionEventModule.kt
@@ -8,18 +8,18 @@ import io.ktor.application.Application
 import kotlinx.coroutines.launch
 
 fun Application.slackReactionEvent(app: App) {
-    app.event(ReactionAddedEvent::class.java) { payload, ctx ->
-        val event = payload.event
-
-        if (event.item.channel == System.getenv("SLACK_THANKS_CHANNEL")) {
-            val repository = ThankRepository()
-            launch {
-                repository.saveReaction(event)
-            }
-        }
-
-        ctx.ack()
-    }
+//    app.event(ReactionAddedEvent::class.java) { payload, ctx ->
+//        val event = payload.event
+//
+//        if (event.item.channel == System.getenv("SLACK_THANKS_CHANNEL")) {
+//            val repository = ThankRepository()
+//            launch {
+//                repository.saveReaction(event)
+//            }
+//        }
+//
+//        ctx.ack()
+//    }
 
     app.event(ReactionRemovedEvent::class.java) { payload, ctx ->
         println("!!!!!!!!!! removed")
