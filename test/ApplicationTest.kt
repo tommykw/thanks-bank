a56775806134de37410892cc6dec4ff71b0b7de4
tommykw <destinys.lotus.1108mn@gmail.com>
2020-08-10 10:33:39 +0900

    Add test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -7,10 +7,18 @@ import kotlin.test.*
 
 class ApplicationTest {
     @Test
-    fun testRequests() = withTestApplication({ module(testing = true)}) {
+    fun testRequests() = withTestApplication({ module(testing = true) }) {
         with(handleRequest(HttpMethod.Post, "/api/thank/daily")) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertEquals("""{"status":"OK"}""", response.content)
+        }
+
+        with(handleRequest(HttpMethod.Get, "/thanks")) {
+            assertEquals(HttpStatusCode.OK, response.status())
+        }
+
+        with(handleRequest(HttpMethod.Get, "/thanks/1")) {
+            assertEquals(HttpStatusCode.OK, response.status())
         }
     }
 }
